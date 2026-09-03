c485e6ac3fcce88f0f8ac664b41d7e78ecce5933
James Wang <jdwwang@gmail.com>
2026-09-03 16:23:36 +1000

    Added functionality to zoom into a particular graph and zoom back out. Next is to merge 3 pages into 1

--- frontend/src/Page1.tsx
+++ frontend/src/Page1.tsx
@@ -4,26 +4,54 @@ import ChartTemplate from './templates/ChartTemplate';
 const WEBSOCKETPORT = 3002;
 
 function Page1() {
+  const [expanded, setExpanded] = useState<string | null>(null);
+
+  const graphs = [
+    {
+      key: 'Wheel Speed',
+      colour: '#06f36f',
+      style: 'p-2 rounded-lg bg-[#280c47] text-[#06f36f]'
+    },
+    {
+      key: 'Throttle position',
+      colour: '#06f36f',
+      style: 'p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'
+    },
+    {
+      key: 'RPM',
+      colour: '#06f36f',
+      style: 'p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'
+    },  
+    {
+      key: 'Motor Temperature',
+      colour: '#280c47',
+      style: 'p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'
+    },  
+  ];
+
   return (
     <>
       <div className='min-h-screen bg-gradient-to-br from-[#06f36f] via-[#280c47] to-[#280c47]'>
         <div className='min-h-screen flex justify-center items-center'>
-          <div className='grid grid-cols-2 gap-8'>
-            <div className='w-140 h-85 p-2 rounded-lg bg-[#280c47] text-[#06f36f]'>
-              <ChartTemplate name='Wheel Speed' port={0} colour='#06f36f'></ChartTemplate>
-            </div>
-            <div className='w-140 h-85 p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'>
-              <ChartTemplate name='Throttle Position' port={0} colour='#06f36f'></ChartTemplate>
-            </div>
-            <div className='w-140 h-85 p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'>
-               <ChartTemplate name='RPM' port={0} colour='#06f36f'></ChartTemplate>
-            </div>
-            <div className='w-140 h-85 p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#06f36f] to-[#06f36f] text-[#280c47]'>
-               <ChartTemplate name='Motor Temperature' port={0} colour='#280c47'></ChartTemplate>
-            </div>
-          </div>
+          { expanded ? ( 
+            <>
+              <div className={`w-[87vw] h-[95vh] ${graphs.find((chart) => chart.key === expanded)!.style}`} onClick={() => setExpanded(null)}>
+                <ChartTemplate name={graphs.find((chart) => chart.key === expanded)!.key} port={0} colour={graphs.find((chart) => chart.key === expanded)!.colour}></ChartTemplate>
+              </div>
+            </>
+            ) : (
+            <>
+              <div className='grid grid-cols-2 gap-8'>
+                {graphs.map(({key, colour, style}) => (
+                  <div key={key} className={`w-140 h-85 ${style}`} onClick={() => setExpanded(key)}>
+                    <ChartTemplate name={key} port={0} colour={colour}></ChartTemplate>
+                  </div>
+                ))}
+              </div>
+            </>
+            )
+          }
         </div>
-
       </div>
     </>
   )
--- frontend/src/Page2.tsx
+++ frontend/src/Page2.tsx
@@ -2,26 +2,53 @@ import { useState, useEffect } from 'react'
 import ChartTemplate from './templates/ChartTemplate';
 
 function Page2() {
+  const [expanded, setExpanded] = useState<string | null>(null);
+  const graphs = [
+    {
+      key: 'Coolant Temperature Motor Inlet',
+      colour: '#06f36f',
+      style: 'p-2 rounded-lg bg-[#280c47] text-[#06f36f]'
+    },
+    {
+      key: 'Coolant Temperature Motor Outlet',
+      colour: '#06f36f',
+      style: 'p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'
+    },
+    {
+      key: 'Battery Temperature',
+      colour: '#06f36f',
+      style: 'p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'
+    },  
+    {
+      key: 'Battery Voltage',
+      colour: '#280c47',
+      style: 'p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'
+    },  
+  ];
+
   return (
     <>
       <div className='min-h-screen bg-gradient-to-br from-[#06f36f] via-[#280c47] to-[#280c47]'>
         <div className='min-h-screen flex justify-center items-center'>
-          <div className='grid grid-cols-2 gap-8'>
-            <div className='w-140 h-85 p-2 rounded-lg bg-[#280c47] text-[#06f36f]'>
-              <ChartTemplate name='Coolant Temperature Motor Inlet' port={0} colour='#06f36f'></ChartTemplate>
-            </div>
-            <div className='w-140 h-85 p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'>
-              <ChartTemplate name='Coolant Temperature Motor Outlet' port={0} colour='#06f36f'></ChartTemplate>
-            </div>
-            <div className='w-140 h-85 p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#280c47] to-[#06f36f] text-[#06f36f]'>
-               <ChartTemplate name='Battery Temperature' port={0} colour='#06f36f'></ChartTemplate>
-            </div>
-            <div className='w-140 h-85 p-2 rounded-lg bg-gradient-to-br from-[#280c47] via-[#06f36f] to-[#06f36f] text-[#280c47]'>
-               <ChartTemplate name='Battery Voltage' port={0} colour='#280c47'></ChartTemplate>
-            </div>
-          </div>
+          { expanded ? ( 
+            <>
+              <div className={`w-[87vw] h-[95vh] ${graphs.find((chart) => chart.key === expanded)!.style}`} onClick={() => setExpanded(null)}>
+                <ChartTemplate name={graphs.find((chart) => chart.key === expanded)!.key} port={0} colour={graphs.find((chart) => chart.key === expanded)!.colour}></ChartTemplate>
+              </div>
+            </>
+            ) : (
+            <>
+              <div className='grid grid-cols-2 gap-8'>
+                {graphs.map(({key, colour, style}) => (
+                  <div key={key} className={`w-140 h-85 ${style}`} onClick={() => setExpanded(key)}>
+                    <ChartTemplate name={key} port={0} colour={colour}></ChartTemplate>
+                  </div>
+                ))}
+              </div>
+            </>
+            )
+          }
         </div>
-
       </div>
     </>
   )
